8caa1da8d91a6f738156bbe25eec805f3ce22288
jliv27 <jace_livigni27@milton.edu>
2026-02-27 12:04:38 -0500

    ClaSsWorK

--- src/MyChomp.java
+++ src/MyChomp.java
@@ -1,7 +1,13 @@
 public class MyChomp {
-    public int id = 0;
-    public int[] boards = new int[19];
-    public int possibleBoards;
+    public int id = 1;
+    public int[] boards = new int[20];
+
+    public int sBoards;
+    public int workBoards;
+
+    public int x;
+    public int y;
+    public int z;
 
     public static void main(String[] args) {
         new MyChomp();
@@ -14,22 +20,33 @@ public class MyChomp {
                    if(x >= y && x >= z && y >= z && x != 0){
                        assert false;
                        boards[id] = (x * 100) + (y * 10) + z;
-                    System.out.println(boards[id] + ": " + id);
+                    System.out.println(boards[id] + ", id: " + id);
                     id++;
                     }
                 }
             }
         }
-        PossibleBoards();
+//        for (int i = 0; i < boards.length; i++) {
+//            PossibleBoards(boards[i]);
+//        }
+        PossibleBoards(boards[0]);
     }
 
-    public void PossibleBoards(){
+    public int PossibleBoards(int pBoards){
 
-        for (int i = 0; i < boards.length; i++) {
-            if (boards[i] < 333){
-               possibleBoards = boards[i] - 10 * i;
-                System.out.println(possibleBoards);
+        x = pBoards/100;
+        y = (pBoards - (x * 100))/10;
+        z = pBoards - ((x * 100) + (y * 10));
+
+        sBoards = x + y + z;
+
+        for (int i = 0; i < sBoards; i++) {
+
+            if(x >= y && x >= z && y >= z && x != 0){
+                System.out.println("Subsequent Board " + i + ": " + (x * 100) + (y * 10) + z);
             }
         }
+
+        return pBoards;
     }
 }
